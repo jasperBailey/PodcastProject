05aa415dcb0269b1ac02eabbb3512e30714b366d
Wenjing2023 <wenjing2018uk@gmail.com>
2023-04-28 15:07:23 +0100

    deleted comments

--- client/src/containers/PodcastContainer.js
+++ client/src/containers/PodcastContainer.js
@@ -5,57 +5,24 @@ import { request, gql, GraphQLClient } from "graphql-request";
 import PodcastService from "../services/PodcastService";
 
 const PodcastContainer = () => {
-    // const apikey = process.env.REACT_APP_KEY;
+  const [favPods, setFavPods] = useState([]);
 
-    // const endpoint = `https://api.taddy.org`;
+  const getFavPods = async () => {
+    const favouritesData = await PodcastService.getFavourites();
+    setFavPods(favouritesData);
+  };
 
-    // const graphQLClient = new GraphQLClient(endpoint, {
-    //     headers: {
-    //         "X-USER-ID": 372,
-    //         "X-API-KEY": apikey,
-    //     },
-    // });
+  useEffect(() => {
+    getFavPods();
+  }, []);
 
-    // const query = gql`
-    //     {
-    //         getPodcastSeries(uuid:"d682a935-ad2d-46ee-a0ac-139198b83bcc")
-            
-    //         {
-    //             uuid
-    //             name
-    //             description
-    //             imageUrl
-    //         }
-    //     }
-    // `;
-
-    // const getData = async () => {
-    //     const data = await graphQLClient.request(query);
-    //     console.log(data);
-    // };
-
-    // useEffect( () => {
-    //     getData()
-    // }, [])
-
-    const [favPods, setFavPods] = useState([])
-
-    const getFavPods = async () => {
-        const favouritesData = await PodcastService.getFavourites()
-        setFavPods(favouritesData)
-    }
-
-    useEffect( () => {
-        getFavPods()
-    }, [])
-
-    return (
-        <>
-            <h2>Podcast Container</h2>
-            {/* <PodcastList /> */}
-            {favPods ? <FavList favPods={favPods}/>: null}
-        </>
-    );
+  return (
+    <>
+      <h2>Podcast Container</h2>
+      {/* <PodcastList /> */}
+      {favPods ? <FavList favPods={favPods} /> : null}
+    </>
+  );
 };
 
 export default PodcastContainer;
