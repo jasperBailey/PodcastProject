54bb42fae938689e4661635e16da3b0d096f95a9
user <user@users-MacBook-Pro.local>
2023-04-28 10:53:16 +0100

    help

--- client/src/containers/PodcastContainer.js
+++ client/src/containers/PodcastContainer.js
@@ -1,16 +1,45 @@
 import PodcastList from "../components/PodcastList";
 import FavList from "../components/FavList";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { request, gql, GraphQLClient } from "graphql-request";
 
 const PodcastContainer = () => {
-
     const apikey = process.env.REACT_APP_KEY;
 
-    return ( <>
-        <h2>Podcast Container</h2>
-        <PodcastList />
-        <FavList/>
-    </> );
-}
- 
+    const endpoint = `https://api.taddy.org`;
+
+    const graphQLClient = new GraphQLClient(endpoint, {
+        headers: {
+            "X-USER-ID": 372,
+            "X-API-KEY": apikey,
+        },
+    });
+
+    const query = gql`
+        {
+            getPodcastSeries(name: "This American Life") {
+                uuid
+                name
+                description
+                imageUrl
+            }
+        }
+    `;
+
+    const getData = async () => {
+        const data = await graphQLClient.request(query);
+        console.log(data);
+    };
+
+    getData();
+
+    return (
+        <>
+            <h2>Podcast Container</h2>
+            <PodcastList />
+            <FavList />
+        </>
+    );
+};
+
 export default PodcastContainer;
