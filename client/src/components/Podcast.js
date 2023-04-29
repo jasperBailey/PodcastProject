aaca8650c369600ea5345ce01ccba001658f2c8c
user <user@8afbfe65.st-andrews.ac.uk>
2023-04-29 10:20:41 +0100

    refactored API call in Podcast

--- client/src/components/Podcast.js
+++ client/src/components/Podcast.js
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react";
 import { getPodSeries } from "../services/APIService";
 
-const Podcast = ({pod}) => {
-
-    const [podcastData, setPodcastData] = useState({})
+const Podcast = ({ pod }) => {
+    const [podcastData, setPodcastData] = useState({});
 
     const fetchPodcastData = async () => {
-        const data = await getPodSeries(pod.uuid)
-        return data
-    }
-    
-    useEffect( () => {
-        const data = fetchPodcastData()
-        setPodcastData(data)
-    }, [])
+        const data = await getPodSeries(pod.uuid);
+        return data;
+    };
 
+    useEffect(() => {
+        const data = fetchPodcastData();
+        setPodcastData(data);
+    }, []);
 
-
-    return ( <>
-        
-
-    </> );
-}
+    return (
+        <>
+            <li>{podcastData.name}</li>
+            <li>{podcastData.description}</li>
+            <img src={podcastData.imageUrl}></img>
+        </>
+    );
+};
 export default Podcast;
