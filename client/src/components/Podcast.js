76b583998f458dfaedb45aee1b1177cbc50b6757
user <user@8afbfe65.st-andrews.ac.uk>
2023-04-29 10:11:09 +0100

    refactored API requests

--- client/src/components/Podcast.js
+++ client/src/components/Podcast.js
@@ -1,38 +1,18 @@
 import { useEffect } from "react";
-import { request, gql, GraphQLClient } from "graphql-request";
+import { getPodSeries } from "../services/APIService";
 
 const Podcast = ({pod}) => {
 
-    const apikey = process.env.REACT_APP_KEY
-
-    const endpoint = `https://api.taddy.org`;
-
-    const graphQLClient = new GraphQLClient(endpoint, {
-        headers: {
-            "X-USER-ID": 372,
-            "X-API-KEY": apikey,
-        },
-    });
-
-    const query = gql`
-        {
-            getPodcastSeries(uuid: "${pod.uuid}") {
-                uuid
-                name
-                description
-                imageUrl
-            }
-        }
-    `;
-
-    const getData = async () => {
-        const data = await graphQLClient.request(query);
+    const getPodData = async () => {
+        const data = await getPodSeries(pod.uuid)
         console.log(data)
-    };
-
+    }
+    
     useEffect( () => {
-        getData()
+        getPodData()
     }, [])
+
+
 
     return ( <>
         <h4>Podcast Item</h4>
