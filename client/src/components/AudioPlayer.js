04b60b336db403d9c095f626229dfb63fd86b61d
jasperBailey <jasperbailey98@gmail.com>
2023-05-01 23:19:00 +0100

    added fontawesome icons and more css

--- client/src/components/AudioPlayer.js
+++ client/src/components/AudioPlayer.js
@@ -1,13 +1,28 @@
-import './AudioPlayer.css'
+import "./AudioPlayer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faBackwardStep,
+    faForwardStep,
+    faArrowRotateLeft,
+    faArrowRotateRight,
+    faCirclePlay,
+} from "@fortawesome/free-solid-svg-icons";
 
 const AudioPlayer = () => {
     return (
-        <div className="audio-player">
-            <h3 id='current-episode'>Episode</h3>
-            <section className='controls'>
-                <p>hI</p>
-            </section>
-            <h4 id='current-episode-author'>Author</h4>
+        <div className="audio-player-banner">
+            <img id="current-episode-img" src="https://play-lh.googleusercontent.com/O8mvDQlw4AwmGfUrh4lviZD_PwwhRHz2etA25F77SbXrm3qEHOt2826aNkKar4D0yw" />
+            <div className="audio-player">
+                <h3 id="current-episode">Now playing: Episode</h3>
+                <section className="controls">
+                    <FontAwesomeIcon icon={faBackwardStep} size="xl" />
+                    <FontAwesomeIcon icon={faArrowRotateLeft} size="xl" />
+                    <FontAwesomeIcon icon={faCirclePlay} size="2xl" />
+                    <FontAwesomeIcon icon={faArrowRotateRight} size="xl" />
+                    <FontAwesomeIcon icon={faForwardStep} size="xl" />
+                </section>
+                <h4 id="current-episode-author">Author</h4>
+            </div>
         </div>
     );
 };
--- client/src/containers/PodcastContainer.js
+++ client/src/containers/PodcastContainer.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useParams,
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useParams,
 } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import SearchList from "../components/SearchList";
@@ -14,101 +14,107 @@ import { getPodSeries } from "../services/APIService";
 import "./PodcastContainer.css";
 
 import {
-  getFavourites,
-  postFavourite,
-  putFavourite,
-  deleteFavourite,
+    getFavourites,
+    postFavourite,
+    putFavourite,
+    deleteFavourite,
 } from "../services/PodcastService";
 
 const PodcastContainer = () => {
-  const [dbFavPods, setDbFavPods] = useState([]);
-  const [podcastsData, setPodcastsData] = useState([]);
-  const [selectedPod, setSelectedPod] = useState(null);
-  //   selectedPod
+    const [dbFavPods, setDbFavPods] = useState([]);
+    const [podcastsData, setPodcastsData] = useState([]);
+    const [selectedPod, setSelectedPod] = useState(null);
+    //   selectedPod
 
-  useEffect(() => fetchAllPodcastData, []);
+    useEffect(() => fetchAllPodcastData, []);
 
-  const fetchAllPodcastData = async () => {
-    const favouritesData = await getFavourites();
-    setDbFavPods(favouritesData);
+    const fetchAllPodcastData = async () => {
+        const favouritesData = await getFavourites();
+        setDbFavPods(favouritesData);
 
-    const allPodcastPromises = favouritesData.map(async (dbPodcast) => {
-      const onePodcastData = await fetchOnePodcastData(dbPodcast.uuid);
-      return onePodcastData;
-    });
+        const allPodcastPromises = favouritesData.map(async (dbPodcast) => {
+            const onePodcastData = await fetchOnePodcastData(dbPodcast.uuid);
+            return onePodcastData;
+        });
 
-    const allPodcastsPromise = Promise.all(allPodcastPromises);
-    const allPodcastsData = await allPodcastsPromise;
-    setPodcastsData(allPodcastsData);
-  };
+        const allPodcastsPromise = Promise.all(allPodcastPromises);
+        const allPodcastsData = await allPodcastsPromise;
+        setPodcastsData(allPodcastsData);
+    };
 
-  const fetchOnePodcastData = async (uuid) => {
-    const data = await getPodSeries(uuid);
-    return data;
-  };
+    const fetchOnePodcastData = async (uuid) => {
+        const data = await getPodSeries(uuid);
+        return data;
+    };
 
-  const removeFavourite = (uuidToDelete) => {
-    const idToDelete = dbFavPods.find((favPod) => {
-      return favPod.uuid === uuidToDelete;
-    })._id;
+    const removeFavourite = (uuidToDelete) => {
+        const idToDelete = dbFavPods.find((favPod) => {
+            return favPod.uuid === uuidToDelete;
+        })._id;
 
-    deleteFavourite(idToDelete).then(
-      setDbFavPods(
-        dbFavPods.filter((favPod) => {
-          return favPod.uuid !== uuidToDelete;
-        })
-      )
-    );
-    setPodcastsData(
-      podcastsData.filter((favPod) => {
-        return favPod.uuid !== uuidToDelete;
-      })
-    );
-  };
+        deleteFavourite(idToDelete);
 
-  // console.log("dbFavPods:", dbFavPods);
-  // console.log("podcastsData:", podcastsData);
+        setDbFavPods(
+            dbFavPods.filter((favPod) => favPod.uuid !== uuidToDelete)
+        );
 
-  const handleOnClick = (podcast) => {
-    const findPodcast = podcastsData.find((podData) => {
-      return (podData.uuid = podcast.uuid);
-    });
-    if (!findPodcast) {
-      return;
-    } else {
-      setSelectedPod(podcast);
-    }
-  };
-  console.log("selectedPod: ", selectedPod);
+        setPodcastsData(
+            podcastsData.filter((favPod) => favPod.uuid !== uuidToDelete)
+        );
+    };
 
-  return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<SearchList />} />
+    // console.log("dbFavPods:", dbFavPods);
+    // console.log("podcastsData:", podcastsData);
 
-        <Route
-          path="/favourites"
-          element={
-            dbFavPods ? (
-              <FavList
-                podcastsData={podcastsData}
-                removeFavourite={removeFavourite}
-                handleOnClick={handleOnClick}
-              />
-            ) : null
-          }
-        />
-        <Route
-          path="/episode"
-          element={selectedPod ? <Episodes selectedPod={selectedPod} /> : null}
-        />
-        {/* 
+    const handleOnClick = (podcast) => {
+        const findPodcast = podcastsData.find((podData) => {
+            return (podData.uuid = podcast.uuid);
+        });
+        if (!findPodcast) {
+            return;
+        } else {
+            setSelectedPod(podcast);
+        }
+    };
+    console.log("selectedPod: ", selectedPod);
+
+    return (
+        <>
+            <div className="app-container">
+                <Router>
+                    <NavBar />
+                    <Routes>
+                        <Route exact path="/" element={<SearchList />} />
+
+                        <Route
+                            path="/favourites"
+                            element={
+                                dbFavPods ? (
+                                    <FavList
+                                        podcastsData={podcastsData}
+                                        removeFavourite={removeFavourite}
+                                        handleOnClick={handleOnClick}
+                                    />
+                                ) : null
+                            }
+                        />
+                        <Route
+                            path="/episode"
+                            element={
+                                selectedPod ? (
+                                    <Episodes selectedPod={selectedPod} />
+                                ) : null
+                            }
+                        />
+                        {/* 
         <Route path="/queue" element={<Queue />} />
         <Route path="liked" element={<Liked />} /> */}
-      </Routes>
-    </Router>
-  );
+                    </Routes>
+                </Router>
+            </div>
+            <AudioPlayer />
+        </>
+    );
 };
 
 export default PodcastContainer;
