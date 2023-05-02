d5a8165c04dc9a109ea00760521279ef0caffb08
jasperBailey <jasperbailey98@gmail.com>
2023-05-02 10:05:12 +0100

    moved stuff about

--- client/src/components/AudioPlayer.js
+++ client/src/components/AudioPlayer.js
@@ -23,6 +23,9 @@ const AudioPlayer = () => {
                 </section>
                 <h4 id="current-episode-author">Author</h4>
             </div>
+            <div id="description-container">
+                <p id="current-episode-description">Episode Description Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
         </div>
     );
 };
