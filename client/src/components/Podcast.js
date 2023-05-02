2a48d5ac4c3688e6528e97d559b2b51fc737cb48
Toby Keech <toby.keech18@gmail.com>
2023-05-02 16:05:46 +0100

    fixes

--- client/src/components/Podcast.js
+++ client/src/components/Podcast.js
@@ -3,11 +3,6 @@ import styled from "styled-components";
 import "./Podcast.css";
 
 const Podcast = ({ podcast, addToFavourite }) => {
-  const [subsButtonMsg, setSubsButtonMsg] = useState("subscribe");
-  const handleClickButton = (podcast) => {
-    addToFavourite(podcast);
-    setSubsButtonMsg("subscribed");
-  };
 
   return (
     <>
