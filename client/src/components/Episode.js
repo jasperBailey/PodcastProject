5d1992999e58945090e4c8347107b154233e0918
Wenjing2023 <wenjing2018uk@gmail.com>
2023-05-03 10:15:59 +0100

    added icons

--- client/src/components/Episode.js
+++ client/src/components/Episode.js
@@ -1,32 +1,41 @@
 import styled from "styled-components";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 const Episode = ({ episode, setNowPlaying, seriesName, seriesImageUrl }) => {
-    const handlePlayEpisode = () => {
-        setNowPlaying({
-            ...episode,
-            series: seriesName,
-            imageUrl: seriesImageUrl,
-        });
-    };
+  const handlePlayEpisode = () => {
+    setNowPlaying({
+      ...episode,
+      series: seriesName,
+      imageUrl: seriesImageUrl,
+    });
+  };
 
-    return (
-        <StyledUl>
-            <StyledLi>{episode.name}</StyledLi>
+  return (
+    <StyledUl>
+      <StyledLi>{episode.name}</StyledLi>
 
-            <StyledLi>
-                <p onClick={handlePlayEpisode}>Play</p>
-            </StyledLi>
-        </StyledUl>
-    );
+      <StyledLi>
+        <span onClick={handlePlayEpisode}>
+          <FontAwesomeIcon icon={faCirclePlay} />
+          &nbsp;&nbsp;
+        </span>
+
+        <span>
+          &nbsp;&nbsp;
+          <FontAwesomeIcon icon={faHeart} />
+        </span>
+      </StyledLi>
+    </StyledUl>
+  );
 };
 
 export default Episode;
 
 export const StyledUl = styled.ul`
-    list-style: none;
+  list-style: none;
 `;
 
 export const StyledLi = styled.li`
-    color: black;
-    padding-left: 0;
+  color: black;
+  padding-left: 0;
 `;
